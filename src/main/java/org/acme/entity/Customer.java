b837b51b343ec67d5132a2e8aea01bc11190be59
Sachin Shirodkar <sshirodk@redhat.com>
2025-04-07 09:11:38 +0000

    Fixed unmarshalling

--- src/main/java/org/acme/entity/Customer.java
+++ src/main/java/org/acme/entity/Customer.java
@@ -1,7 +1,9 @@
 package org.acme.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
 
+@Entity
 public class Customer extends PanacheEntity {
 
     public String name;
